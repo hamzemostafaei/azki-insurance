1d72eccbcdebfeb7031b7e42f089abfad9efbc84
hamze <hamze.mostafaei@gmail.com>
2025-03-12 17:08:48 +0330

    Removed unneeded annotation

--- common/common-domain/common-domain-application-api/src/main/java/com/azki/insurance/domain/api/input/CommandHandler.java
+++ common/common-domain/common-domain-application-api/src/main/java/com/azki/insurance/domain/api/input/CommandHandler.java
@@ -5,9 +5,7 @@ import com.azki.insurance.domain.api.command.CommandResult;
 import jakarta.validation.Valid;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 
-@Validated
 @Transactional(propagation = Propagation.REQUIRED)
 public interface CommandHandler<C extends Command, R extends CommandResult<?>> {
 
